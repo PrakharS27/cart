bdd3bbf9e83c833d74b2903ae0fdb3ec065e01b4
Prakhar Srivastava <prakharshivam206@gmail.com>
2022-10-25 00:36:26 +0530

    add increaseQuantity fun

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,36 @@
 import React from 'react';
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title:'Phone',
+            qty: 1,
+            img:''
+        }
+
+    }
+    increaseQuantity = () => {
+        console.log('this', this.state);
+      }
+
     render(){
+        const{price,title,qty}=this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                 <img style={style.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
-                    <div className="cart-item-action">
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>{price}</div>
+                    <div style={{color:'#777'}}>{qty}</div>
+                    <div className="cart-item-actions">
                         {/*Buttons*/}
+                        <img alt="increase" className='action-icons' src="https://img.icons8.com/ios/344/plus--v1.png"
+                        onClick={this.increaseQuantity}/>
+                        <img alt="decrease" className='action-icons' src="https://img.icons8.com/ios/344/minus.png"/>
+                        <img alt="delete" className='action-icons' src="https://img.icons8.com/carbon-copy/344/filled-trash.png"/>
                     </div>
                     <div></div>
 
